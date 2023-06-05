0c9002d442134fdf9f6b3129de8d03ec8647a7fb
Henrique <henriqueveigamoreira@gmail.com>
2023-06-05 20:43:21 -0300

    feat styled-components

--- astrovents/App.tsx
+++ astrovents/App.tsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
+
 import Login from './src/sreens/Login';
 import Start from './src/sreens/Start';
+import Theme from './src/theme';
 
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
-      <Login />
+      <ThemeProvider theme={Theme}>
+        <StatusBar style="auto" />
+        <Login />
+      </ThemeProvider>
     </>
 
   );
